26f1c3b5f1ed6ee459b0e854e4498d28c631fa0c
maximiliano lopez <massilo.87@gmail.com>
2023-07-03 19:02:41 -0300

    img en cloudinary

--- src/components/common/counter/Counter.jsx
+++ src/components/common/counter/Counter.jsx
@@ -1,20 +1,27 @@
-import { Button } from "@mui/material"
-
+import { Button } from "@mui/material";
 
 const Counter = ({ counter, setCounter }) => {
-    return (
-        <div>
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <Button
+          disabled={counter <= 1}
+          variant="contained"
+          onClick={() => setCounter(counter - 1)}
+        >
+          -
+        </Button>
+        <h3>{counter}</h3>
+        <Button variant="contained" onClick={() => setCounter(counter + 1)}>
+          +
+        </Button>
+      </div>
 
-            <div style={{ display: "flex" }}>
-                <Button disabled={counter <= 1} variant="contained" onClick={() => setCounter(counter - 1)}>-</Button>
-                <h3>{counter}</h3>
-                <Button variant="contained" onClick={() => setCounter(counter + 1)}>+</Button>
-            </div>
+      <Button variant="contained" color="secondary">
+        Agregar al carrito
+      </Button>
+    </div>
+  );
+};
 
-            <Button variant="contained" color="secondary">Agregar al carrito</Button>
-
-        </div>
-    )
-}
-
-export default Counter
+export default Counter;
--- src/productsMocks.js
+++ src/productsMocks.js
@@ -1,39 +1,38 @@
-export const products =  [
-    {
-        id: 1,
-        title: "Lámpara simple",
-        price: 1500,
-        stock: 4,
-        description: "Lámparas con iluminación led",
-        category: "Lámparas",
-        img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png"
-    },
-    {
-        id: 2,
-        title: "Lámpara custom",
-        price: 2500,
-        stock: 5,
-        description: "Lámparas a medida",
-        category: "Lámparas",
-        img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430010/foc364fsakkzmausrovn.jpg"
-    },
-    {
-        id: 3,
-        title: "Maceta pequeña",
-        price: 500,
-        stock: 0,
-        description: "Macetas de 8 cm de altura",
-        category: "Macetas",
-        img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430165/images_lujpmr.jpg"
-    },
-    {
-        id: 4,
-        title: "Maceta de jardín",
-        price: 1000,
-        stock: 5,
-        description: "Macetas grandes",
-        category: "Macetas",
-        img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669430220/rtokwmt3j03bblbfhwhb.jpg"
-    }
-    
-]
+export const products = [
+  {
+    id: 1,
+    title: "Lámpara simple",
+    price: 1500,
+    stock: 4,
+    description: "Lámparas con iluminación led",
+    category: "Lámparas",
+    img: "https://res.cloudinary.com/dwiiarrki/image/upload/v1688421305/images_n6dnyi.jpg",
+  },
+  {
+    id: 2,
+    title: "Lámpara custom",
+    price: 2500,
+    stock: 5,
+    description: "Lámparas a medida",
+    category: "Lámparas",
+    img: "https://res.cloudinary.com/dwiiarrki/image/upload/v1688421383/D_NQ_NP_810960-MLA42876364358_072020-O_xwzvqt.webp",
+  },
+  {
+    id: 3,
+    title: "Maceta pequeña",
+    price: 500,
+    stock: 0,
+    description: "Macetas de 8 cm de altura",
+    category: "Macetas",
+    img: "https://res.cloudinary.com/dwiiarrki/image/upload/v1688421450/macetas-plantas-interior-suculentas-Madera2Breciclada252C2BBiopl25C325A1stico252C2B2B2528225292B850p_fo7pkg.jpg",
+  },
+  {
+    id: 4,
+    title: "Maceta de jardín",
+    price: 1000,
+    stock: 5,
+    description: "Macetas grandes",
+    category: "Macetas",
+    img: "https://res.cloudinary.com/dwiiarrki/image/upload/v1688421530/il_1588xN.2321570558_duun-768x1024_idiyin.jpg",
+  },
+];
