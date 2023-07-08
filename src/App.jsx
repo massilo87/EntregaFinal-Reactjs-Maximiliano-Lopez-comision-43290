70228acf77981b1134c698d9e76118c219001ab3
maximiliano lopez <massilo.87@gmail.com>
2023-07-08 15:28:17 -0300

    ajusto error en merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<NavBar />}>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route
+            path="/category/:categoryName"
+            element={<ItemListContainer />}
+          />
+          <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+        </Route>
+
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
