c0da0644e48d686871210fc966a978f2e2da70e3
Maximiliano Lopez <massilo.87@gmail.com>
2023-08-01 19:38:21 -0300

    variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCXB8rpbBx9XhdU_CAjSR56C6ib4RDgMcM",
-  authDomain: "proyecto-react-com43290-ml.firebaseapp.com",
-  projectId: "proyecto-react-com43290-ml",
-  storageBucket: "proyecto-react-com43290-ml.appspot.com",
-  messagingSenderId: "582082028858",
-  appId: "1:582082028858:web:d70eff1e0f56d47dea5c03"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -18,7 +18,10 @@ const AppRouter = () => {
                 <Route path="/dashboard" element={<Dashboard />} />
             </Route>
 
-            <Route path="*" element={<h1>404 - Not Found</h1>} />
+            <Route path="*" element={<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <h1>404 - Not Found</h1>
+                <img src="https://res.cloudinary.com/dwiiarrki/image/upload/v1690927480/404_i8hlli.jpg" alt="notFound" style={{ width: "100%", maxWidth: "400px", alignSelf: "center" }} />
+            </div>} />
         </Routes>
     )
 }
