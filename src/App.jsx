d4587c1fc5926ba1b2129f42ea13402760f35ea3
Maximiliano Lopez <massilo.87@gmail.com>
2023-08-02 23:02:56 -0300

    ajuste de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./router/Approuter";
+import AppRouter from "./router/AppRouter";
 import CartContextCoponent from "./context/CartContext";
 
 function App() {
